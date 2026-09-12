81ff324fc5b53f4e3f8daca024799aaba9704b63
Tessa Painter <tessa@tessapainter.com>
2026-09-12 06:56:51 -0500

    Deploy site

--- _nuxt/static/1789213844/project/businesscardparser/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1789213844",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproject\u002Fbusinesscardparser",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a9698428"}}}}(null));
